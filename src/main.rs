dfc6cdf383519b0d9e02ef2833df3188da78ce1e
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2023-11-10 19:19:15 +0200

    Added a simple welcoming banner.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("===================");
+    println!("=== Tic-Tac-Toe ===");
+    println!("===================");
+    println!();
+    println!("Please enter a selection:");
+    println!("[1] Play");
+    println!("[2] Quit");
+
+    let mut input = String::new();
+    match io::stdin().read_line(&mut input) {
+        Ok(_) => println!("input: {input}"),
+        Err(error) => println!("error: {error}"),
+    }
 }
