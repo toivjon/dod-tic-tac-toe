cdbad1e7c2209db04cd06a25017c6b4e81006739
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2023-11-10 19:34:29 +0200

    Added a support to quit the game.

--- src/main.rs
+++ src/main.rs
@@ -16,10 +16,15 @@ fn show_menu() {
     let mut input = String::new();
     match io::stdin().read_line(&mut input) {
         Ok(_) => match input.trim() {
-            "1" => println!("ONE!"),
-            "2" => println!("TWO!"),
+            "1" => play([' '; 9]),
+            "2" => println!("Bye!"),
             _ => show_menu(),
         },
         Err(error) => println!("error: {error}"),
     }
+}
+
+fn play(grid: [char; 9]) {
+    println!();
+    println!("{:?}", grid);
 }
