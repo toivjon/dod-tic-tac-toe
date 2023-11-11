84c729160520ab52febd710ee33dbf0836d5e3f8
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2023-11-11 21:12:15 +0200

    Moved application logic away from the main.rs.

---
+++ src/lib.rs
@@ -0,0 +1,48 @@
+use std::io;
+
+pub fn show_menu() {
+    println!("===================");
+    println!("=== Tic-Tac-Toe ===");
+    println!("===================");
+    println!();
+    println!("Please enter a selection:");
+    println!("[1] Play");
+    println!("[2] Quit");
+
+    let mut input = String::new();
+    match io::stdin().read_line(&mut input) {
+        Ok(_) => match input.trim() {
+            "1" => play([' '; 9], 'O'),
+            "2" => println!("Bye!"),
+            _ => show_menu(),
+        },
+        Err(error) => println!("error: {error}"),
+    }
+}
+
+fn play(grid: [char; 9], player: char) {
+    assert!(player == 'O' || player == 'X'); // TODO replace with a type.
+    println!();
+    println!("Current turn: {}", player);
+    println!("  | A | B | C |");
+    println!("------------------");
+    println!("1 | {} | {} | {} | 1", grid[0], grid[1], grid[2]);
+    println!("------------------");
+    println!("2 | {} | {} | {} | 2", grid[3], grid[4], grid[5]);
+    println!("------------------");
+    println!("3 | {} | {} | {} | 3", grid[6], grid[7], grid[8]);
+    println!("------------------");
+    println!("  | A | B | C |");
+    println!("");
+    println!("Please enter a cell e.g. 'B2':");
+
+    let mut input = String::new();
+    match io::stdin().read_line(&mut input) {
+        Ok(_) => match input.trim() {
+            "1" => play([' '; 9], 'O'),
+            "2" => println!("Bye!"),
+            _ => show_menu(),
+        },
+        Err(error) => println!("error: {error}"),
+    }
+}
