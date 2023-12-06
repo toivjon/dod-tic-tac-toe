876434e53496fa40931ee6043fea3fcf2ab00964
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2023-12-06 11:20:37 +0200

    Moved output function into own named function in main.rs.

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,15 @@ use std::io;
 use tic_tac_toe::run;
 
 fn main() {
-    run(|output| println!("{output}"), input)
+    run(output, input)
 }
 
+// An output channel that simply writes a line to standard out.
+fn output(value: &str) {
+    println!("{value}")
+}
+
+// An input channel that simple reads a line from standard in.
 fn input() -> String {
     let mut input = String::new();
     match io::stdin().read_line(&mut input) {
