92e51fb5b69b98117e25d1f0b60a596c845b0fce
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2023-12-26 13:06:42 +0200

    Simplified the core run function.

--- src/lib.rs
+++ src/lib.rs
@@ -11,20 +11,15 @@ pub enum Command {
 
 // Run the game with the provided input and output channels.
 pub fn run(output: fn(&str), input: fn() -> String) {
-    let mut command = Option::Some(Command::OpenMainMenu);
-    while command.is_some() {
-        command = execute_command(&command.unwrap(), output, input);
-    }
-}
-
-// Execute the given command possibly leading into a next command to be executed.
-fn execute_command(command: &Command, output: fn(&str), input: fn() -> String) -> Option<Command> {
-    match command {
-        Command::OpenMainMenu => Some(main_menu::run(output, input)),
-        Command::OpenTurnMenu => Some(turn_menu::run(output, input)),
-        Command::Exit => {
-            output("Bye!");
-            Option::None
+    let mut command = Command::OpenMainMenu;
+    loop {
+        match command {
+            Command::OpenMainMenu => command = main_menu::run(output, input),
+            Command::OpenTurnMenu => command = turn_menu::run(output, input),
+            Command::Exit => {
+                output("Bye!");
+                break;
+            }
         }
     }
 }
