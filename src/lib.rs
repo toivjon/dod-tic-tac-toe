f4a0e57a792bf80914f5439865f5588c48ef54f7
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2023-12-26 13:04:37 +0200

    Be more consistent with main command action names.

--- src/lib.rs
+++ src/lib.rs
@@ -4,14 +4,14 @@ mod turn_menu;
 // An enumeration of all available commands within the game.
 #[derive(Debug, PartialEq)]
 pub enum Command {
-    MainMenu,
+    OpenMainMenu,
     OpenTurnMenu,
     Exit,
 }
 
 // Run the game with the provided input and output channels.
 pub fn run(output: fn(&str), input: fn() -> String) {
-    let mut command = Option::Some(Command::MainMenu);
+    let mut command = Option::Some(Command::OpenMainMenu);
     while command.is_some() {
         command = execute_command(&command.unwrap(), output, input);
     }
@@ -20,7 +20,7 @@ pub fn run(output: fn(&str), input: fn() -> String) {
 // Execute the given command possibly leading into a next command to be executed.
 fn execute_command(command: &Command, output: fn(&str), input: fn() -> String) -> Option<Command> {
     match command {
-        Command::MainMenu => Some(main_menu::run(output, input)),
+        Command::OpenMainMenu => Some(main_menu::run(output, input)),
         Command::OpenTurnMenu => Some(turn_menu::run(output, input)),
         Command::Exit => {
             output("Bye!");
